3392c6740bae384b87899aab0a5cea27fa446603
hanankalman <hanankalman@gmail.com>
2023-04-02 18:09:50 +0300

    file update

--- FullStackDevCorse/react/frontend/src/screen/SigninScreen.js
+++ FullStackDevCorse/react/frontend/src/screen/SigninScreen.js
@@ -31,7 +31,7 @@ export default function SigninScreen() {
             });
             ctxDispatch({ type: 'USER_SIGNIN', payload: data });
             localStorage.setItem('userInfo', JSON.stringify(data));
-            Navigate(redirect || '/');
+            //Navigate(redirect || '/');
         } catch (err) {
             toast.error(getError(err));
         }
