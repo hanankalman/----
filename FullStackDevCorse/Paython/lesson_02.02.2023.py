f2638524c449c9e062c08661e9fed370782deb78
hanankalman <hanankalman@gmail.com>
2023-04-02 09:54:20 +0300

    file update

---
+++ FullStackDevCorse/Paython/lesson_02.02.2023.py
@@ -0,0 +1,116 @@
+#What is the output of the following code:
+
+
+print(len("Hello, World!"))
+
+# Output: 
+
+#What is the output of the following code: 13
+
+
+print("Hello, World!"[0:5])
+
+# Output: 
+
+#What is the output of the following code: Hello
+
+
+text = "Hello, World!"
+print(text.replace("Hello", "Hi"))
+
+# Output: 
+
+#    What is the output of the following code: hi, world
+
+text = "Hello, World!"
+print(text.isdigit())
+
+# Output: 
+
+#What is the output of the following code: nothing
+
+
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers))
+
+# Output: 
+
+#What is the output of the following code: 15
+
+
+
+numbers = [1, 2, 3, 4, 5]
+print(min(numbers))
+
+# Output: 
+
+#What is the output of the following code: 1
+
+
+numbers = [1, 2, 3, 4, 5]
+print(max(numbers))
+
+# Output:
+
+#What is the output of the following code: 5
+
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[::-1])
+
+# Output: 
+
+#What is the output of the following code: 5
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[0:3])
+
+# Output: 
+
+#What is the output of the following code: 1 2 3
+
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+# Output:
+
+# What is the output of the following code: 1 2 3 4 5 6
+
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(0, 0)
+print(numbers)
+
+# Output:
+
+#    What is the output of the following code: 0 1 2 3 4 5 
+
+
+numbers = [1, 2, 3, 4, 5]
+numbers.remove(3)
+print(numbers)
+
+# Output: 
+
+#    What is the output of the following code: 1 2 4 5
+
+numbers = [1, 2, 3, 4, 5]
+numbers.pop()
+print(numbers)
+
+#Output: 1 2 3 4
+
+#    What is the output of the following code:
+
+numbers = [1, 2, 3, 4, 5]
+numbers.clear()
+print(numbers)
+
+#Output:
+
+#    What is the output of the following code: nothing
+
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))
+
+#Output: 5
---
+++ FullStackDevCorse/react/backend/server.js
@@ -0,0 +1,50 @@
+
+import express from 'express';
+//import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import productRouter from './routes/productRoutes.js';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+    .then(() => {
+        console.log('connected to db');
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
+
+const app = express();
+
+app.use('/api/products', productRouter);
+
+// app.get('/api/products', (req, res) => {
+//     res.send(data.products);
+// })
+
+// app.get('/api/products/:slug', (req, res) => {
+//     const product = data.products.find((x) => x.slug === req.params.slug);
+//     if (product) {
+//         res.send(product);
+//     } else {
+//         res.status(404).send({ message: 'Product Not Found' });
+//     }
+// });
+
+// app.get('/api/product/:id', (req, res) => {
+//     const product = data.products.find((x) => x._id === req.params.id);
+//     if (product) {
+//         res.send(product);
+//     } else {
+//         res.status(404).send({ message: 'Product Not Found' });
+//     }
+// });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`server running on http://localhost:${port}`)
+})
